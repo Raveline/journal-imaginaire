a8f9fef1596575fdacda42bef39cd083c1aded67
Raveline <eraveline@gmail.com>
2014-11-10 18:19:34 +0100

    Next / previous.

--- site.hs
+++ site.hs
@@ -1,10 +1,17 @@
---------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
+--------------------------------------------------------------------------------
 import              Data.Monoid (mappend)
 import              Hakyll
 import              Data.Functor ((<$>))
+import              Data.List (sortBy, intercalate)
+import              System.FilePath (takeFileName)
+import              Data.Time.Format (parseTime)
+import              System.Locale (defaultTimeLocale)
+import              Data.Time.Clock (UTCTime)
+import              Control.Applicative
 
 --------------------------------------------------------------------------------
+
 main :: IO ()
 main = hakyll $ do
     match "images/*" $ do
@@ -58,6 +65,8 @@ main = hakyll $ do
 postCtx :: Context String
 postCtx =
     dateField "date" "%d/%m/%Y" `mappend`
+    field "nextPost" nextPostUrl `mappend`
+    field "prevPost" previousPostUrl `mappend`
     defaultContext
 
 feedCtx :: Context String
@@ -75,3 +84,46 @@ feedConfiguration = FeedConfiguration
     , feedRoot        = "http://self.eraveline.eu"
     }
 
+--------------------------------------------------------------------------------
+-- Previous / next urls
+postsGlob = "posts/*"
+
+previousPostUrl :: Item String -> Compiler String
+previousPostUrl post = do
+    posts <- getMatches postsGlob
+    let ident = itemIdentifier post
+        sortedPosts = sortIdentifiersByDate posts
+        ident' = itemBefore sortedPosts ident
+    case ident' of
+        Just i -> (fmap (maybe empty $ toUrl) . getRoute) i
+        Nothing -> empty
+
+nextPostUrl :: Item String -> Compiler String
+nextPostUrl post = do
+    posts <- getMatches postsGlob
+    let ident = itemIdentifier post
+        sortedPosts = sortIdentifiersByDate posts
+        ident' = itemAfter sortedPosts ident
+    case ident' of
+        Just i -> (fmap (maybe empty $ toUrl) . getRoute) i
+        Nothing -> empty
+
+itemAfter :: Eq a => [a] -> a -> Maybe a
+itemAfter xs x = lookup x $ zip xs (tail xs)
+
+itemBefore :: Eq a => [a] -> a -> Maybe a
+itemBefore xs x =
+    lookup x $ zip (tail xs) xs
+
+urlOfPost :: Item String -> Compiler String
+urlOfPost = fmap (maybe empty $ toUrl) . getRoute . itemIdentifier
+
+sortIdentifiersByDate :: [Identifier] -> [Identifier]
+sortIdentifiersByDate identifiers =
+    reverse $ sortBy byDate identifiers
+        where
+            byDate id1 id2 =
+                let fn1 = takeFileName $ toFilePath id1
+                    fn2 = takeFileName $ toFilePath id2
+                    parseTime' fn = parseTime defaultTimeLocale "%Y-%m-%d" $ intercalate "-" $ take 3 $ splitAll "-" fn
+                in compare ((parseTime' fn1) :: Maybe UTCTime) ((parseTime' fn2) :: Maybe UTCTime)
