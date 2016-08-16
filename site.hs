7850ef1472aadb27fc0979187ccc6978c51226bf
Raveline <eraveline@gmail.com>
2016-08-16 16:10:56 +0200

    Numerous additions.
    - Stack fix for time libs.
    - Chronological order rather than recent first for post list.
    - Series begin / end handling.

--- site.hs
+++ site.hs
@@ -1,14 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 --------------------------------------------------------------------------------
-import              Data.Monoid (mappend)
-import              Hakyll
-import              Data.Functor ((<$>))
-import              Data.List (sortBy, intercalate)
-import              System.FilePath (takeFileName)
-import              Data.Time.Format (parseTime)
-import              System.Locale (defaultTimeLocale)
-import              Data.Time.Clock (UTCTime)
-import              Control.Applicative
+import Data.Maybe (fromMaybe, isNothing, isJust)
+import Data.Monoid (mappend)
+import Hakyll
+import Data.Functor ((<$>))
+import Data.List (sortBy, intercalate)
+import System.FilePath (takeFileName)
+import Data.Time.Format (parseTime, defaultTimeLocale)
+import Data.Time.Clock (UTCTime)
+import Control.Applicative
 
 --------------------------------------------------------------------------------
 
@@ -39,7 +39,7 @@ main = hakyll $ do
     create ["index.html"] $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
+            posts <- chronological =<< loadAll "posts/*"
             let archiveCtx =
                     listField "posts" postCtx (return posts) `mappend`
                     constField "title" "Billets"             `mappend`
@@ -73,6 +73,8 @@ postCtx =
     dateField "date" "%d/%m/%Y" `mappend`
     field "nextPost" nextPostUrl `mappend`
     field "prevPost" previousPostUrl `mappend`
+    field "seriesBegin" beginsSeries `mappend`
+    field "seriesEnd" endsSeries `mappend`
     defaultContext
 
 feedCtx :: Context String
@@ -114,6 +116,40 @@ nextPostUrl post = do
         Just i -> (fmap (maybe empty $ toUrl) . getRoute) i
         Nothing -> empty
 
+-- This is the beginning of a series if the item published
+-- AFTER THIS ONE had no serie and this one had.
+beginsSeries :: Item String -> Compiler String
+beginsSeries post = do
+  posts <- getMatches postsGlob
+  let ident = itemIdentifier post
+      sortedPosts = sortIdentifiersByDate posts
+      newerItem = itemAfter sortedPosts ident
+  newerItemSeries <- getSeriesTag newerItem
+  currentItemSeries <- getSeriesTag (Just ident)
+  if (isNothing newerItemSeries && isJust currentItemSeries)
+    then return $ fromMaybe empty currentItemSeries
+    else empty
+
+-- This is the beginning of a series if the item published
+-- BEFORE THIS ONE had no serie and this one had.
+endsSeries :: Item String -> Compiler String
+endsSeries post = do
+  posts <- getMatches postsGlob
+  let ident = itemIdentifier post
+      sortedPosts = sortIdentifiersByDate posts
+      olderItem = itemBefore sortedPosts ident
+  olderItemSeries <- getSeriesTag olderItem
+  currentItemSeries <- getSeriesTag (Just ident)
+  if (isNothing olderItemSeries && isJust currentItemSeries)
+    then return $ fromMaybe empty currentItemSeries
+    else empty
+
+getSeriesTag :: Maybe Identifier -> Compiler (Maybe String)
+getSeriesTag Nothing = return Nothing
+getSeriesTag (Just i)= do f <- getMetadataField i "serie"
+                          return f
+
+
 itemAfter :: Eq a => [a] -> a -> Maybe a
 itemAfter xs x = lookup x $ zip xs (tail xs)
 
@@ -131,5 +167,6 @@ sortIdentifiersByDate identifiers =
             byDate id1 id2 =
                 let fn1 = takeFileName $ toFilePath id1
                     fn2 = takeFileName $ toFilePath id2
-                    parseTime' fn = parseTime defaultTimeLocale "%Y-%m-%d" $ intercalate "-" $ take 3 $ splitAll "-" fn
+                    parseTime' fn = parseTime defaultTimeLocale "%d-%d-%Y" $ intercalate "-" $ take 3 $ splitAll "-" fn
                 in compare ((parseTime' fn1) :: Maybe UTCTime) ((parseTime' fn2) :: Maybe UTCTime)
+
